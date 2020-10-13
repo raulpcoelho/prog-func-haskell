874e5442aa458916a7540bd7f65888cf6744b5b3
Raul Coelho <rpc3@cin.ufpe.br>
2020-10-13 01:35:34 -0300

    README

--- l1/q4/q4.hs
+++
@@ -1,9 +0,0 @@
-decEnigma :: String -> [(Char, Char)] -> String
-decEnigma str alphabet = [b | x <- str, (a, b) <- alphabet, x == a]
-
-main :: IO ()
-main = do
-    a <- getLine
-    b <- getLine
-    let result = decEnigma a (read b)
-    print result
