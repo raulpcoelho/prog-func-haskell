a949553de8fc4ef89d0ebd2d58a14fa60f3feb12
Raul Coelho <rpc3@cin.ufpe.br>
2020-10-13 01:38:35 -0300

    l1

---
+++ l1/q4/q4.hs
@@ -0,0 +1,9 @@
+decEnigma :: String -> [(Char, Char)] -> String
+decEnigma str alphabet = [b | x <- str, (a, b) <- alphabet, x == a]
+
+main :: IO ()
+main = do
+    a <- getLine
+    b <- getLine
+    let result = decEnigma a (read b)
+    print result
